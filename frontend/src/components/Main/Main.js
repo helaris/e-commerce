e518bfaf1394b3e682798b7990a3efa7ae44404d
helaris <helari.sosi@gmail.com>
2021-04-16 17:02:48 +0200

    Fix error message position main page

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -27,28 +27,26 @@ const Main = ({ addToCart }) => {
   return (
     <section className="main__container">
       <h1 className="main__container-header">Our bestsellers</h1>
-      <div className="main__products">
-        {error ? (
-          <h2 className="error">
-            Sorry something went wrong, please try again later.
-          </h2>
-        ) : (
-          <>
-            {recommended.map((product) => (
-              <ProductCard
-                key={product.id}
-                id={product.id}
-                name={product.name}
-                description={product.description}
-                defaultImage={product.defaultImage}
-                price={product.price}
-                addToCart={addToCart}
-                product={product}
-              />
-            ))}
-          </>
-        )}
-      </div>
+      {error ? (
+        <h2 className="error">
+          Sorry something went wrong, please try again later.
+        </h2>
+      ) : (
+        <div className="main__products">
+          {recommended.map((product) => (
+            <ProductCard
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              description={product.description}
+              defaultImage={product.defaultImage}
+              price={product.price}
+              addToCart={addToCart}
+              product={product}
+            />
+          ))}
+        </div>
+      )}
     </section>
   );
 };
